11b90510dfe30f1979c304a36fe4e3dac25fed47
ha <typ0@naver.com>
2017-12-08 01:59:34 +0900

    feat: prevent submit after expansion release

--- src/api/controller/upload/index.ts
+++ src/api/controller/upload/index.ts
@@ -59,6 +59,14 @@ export class UploadController {
   };
 
   add = async ({ body }: Request, res: Response, tx: EntityManager) => {
+    const now = new Date().getTime();
+    const nextRelease = 1512691200000;
+    if (now > nextRelease) {
+      return res.badRequest({
+        message: '확장팩이 발매되어 더이상 평가할 수 없습니다.',
+      });
+    }
+
     const { error, value } = uploadValidator(body);
     if (error) {
       return res.badRequest({
